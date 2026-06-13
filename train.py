259e26e8418a8b113c3507c983024f983314d1c8
devisricharanp-ux <devisricharanp@gmail.com>
2026-06-13 20:59:01 +0530

    Update train.py

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         batch=16,          # Lowering to 16 completely stabilizes VRAM on an 8GB card
         imgsz=640,         
         workers=2,         # Keeps CPU dataloader system RAM light
-        cache=False,       
+        cache=False,        # cache images in RAM if dataset fits (speeds up a lot)
         overlap_mask=True, # Forces overlapping masks onto one canvas to save memory
         max_det=50,        # Reduces max evaluation objects per image from 300 to 50
         val=True,          # Keep validation enabled
